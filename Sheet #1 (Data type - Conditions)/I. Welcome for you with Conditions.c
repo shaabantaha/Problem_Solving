2d17f7d6fac3ad3b1d6294b98e92eeffab2fda1d
shaaban <engshaabantaha@gmail.com>
2023-09-01 18:12:33 +0300

    PROBLEM I. Welcome for you with Conditions

---
+++ Sheet #1 (Data type - Conditions)/I. Welcome for you with Conditions.c
@@ -0,0 +1,14 @@
+/****************************I. Welcome for you with Conditions*************************/
+
+
+#include  <stdio.h>
+
+int main()
+{
+    char A, B;
+    scanf("%d %d", &A, &B);
+
+    A >= B ? printf("Yes") : printf("No");
+
+    return 0;
+}
