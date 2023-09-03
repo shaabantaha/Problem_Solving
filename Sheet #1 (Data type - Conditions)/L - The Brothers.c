a79e303df3d9c7ac2b542106741fe8e0fe9cd405
shaaban <engshaabantaha@gmail.com>
2023-09-03 20:43:22 +0300

    PROBLEM L - The Brothers

---
+++ Sheet #1 (Data type - Conditions)/L - The Brothers.c
@@ -0,0 +1,51 @@
+/****************************L. The Brothers*************************/
+
+#include <stdio.h>
+
+int main() {
+    char F1[100];
+    char S1[100];
+    char F2[100];
+    char S2[100];
+
+   // printf("Enter the first and last names for the first person: ");
+
+    /*We can use `sizeof` in `scanf` to specify the maximum buffer size and prevent buffer overflow, ensuring that the input does not exceed the allocated memory for the array.*/
+	scanf("%s %s", F1, S1);
+
+    //printf("Enter the first and last names for the second person: ");
+	scanf("%s %s", F2, S2);
+
+    //printf("Null Terminator of S1: %c\n", S1[strlen(S1)]);
+    // Now, you have the first and last names of two people in F1, S1, F2, and S2
+    
+
+    char* S1Ptr = &(S1[0]);
+    char* S2Ptr = &(S2[0]);
+    unsigned char IsTrue = 1;
+
+    while (*S1Ptr != '\0')
+    {
+        if (*S1Ptr == *S2Ptr)
+        {
+            S1Ptr++;
+            S2Ptr++;
+        }
+        else
+        {
+            IsTrue = 0;
+            break;
+        }
+    }
+
+    if (IsTrue)
+    {
+        printf("ARE Brothers");
+    }
+    else
+    {
+        printf("NOT");
+    }
+    return 0;
+}
+
